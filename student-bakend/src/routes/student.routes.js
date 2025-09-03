82088c3e4c8970c8ebbb6c5dc3fc92ca6873df8e
KALYANISHARNAGATCS <sharnagatkalyani0812@gmail.com>
2025-09-03 23:14:19 +0530

    delete unwanted files

--- student-bakend/src/routes/student.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const studentController = require("../controllers/student.controller");
-
-router.post("/", studentController.createStudent);
-
-
-module.exports = router;
